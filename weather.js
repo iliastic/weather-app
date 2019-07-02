2f4c75b00b7800d50029777f5e1ab5629471e332
iliastic <mesbahiilias1@gmail.com>
2019-07-02 09:23:16 +0200

    got my key, made var for input&button

---
+++ weather.js
@@ -0,0 +1,11 @@
+const key='d7ee636e32ea15c1e10edd173dfc9e91';
+let input=document.getElementsByClassName('search');
+let button=document.getElementById('search-btn');
+
+button.addEventListener('click', () =>{
+
+});
+
+
+
+api.openweathermap.org/data/2.5/forecast?q={city name},{country code} 
