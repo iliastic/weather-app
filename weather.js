0f278ba9d017eed38355a09b80e36710d269613f
iliastic <mesbahiilias1@gmail.com>
2019-07-02 09:50:11 +0200

    added eventListener to button

--- weather.js
+++ weather.js
@@ -1,11 +1,13 @@
 const key='d7ee636e32ea15c1e10edd173dfc9e91';
-let input=document.getElementsByClassName('search');
+let input=document.getElementById('search');
 let button=document.getElementById('search-btn');
 
-button.addEventListener('click', () =>{
+button.addEventListener('click',() => {
+let city= input.value;
 
 });
 
 
 
-api.openweathermap.org/data/2.5/forecast?q={city name},{country code} 
+
+// /api.openweathermap.org/data/2.5/forecast?q={city name},{country code} 
