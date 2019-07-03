bfb4c0ccfcfae6726e8907e946ac5ff53af5d6c5
iliastic <mesbahiilias1@gmail.com>
2019-07-03 16:10:20 +0200

    fixed gradient, colors, etc...

--- weather.js
+++ weather.js
@@ -1,6 +1,7 @@
 const key='e9a2a366f60242dd2238a9c477ce36d3';
 let input=document.getElementById('search');
 let button=document.getElementById('search-btn');
+let iconWeather=document.getElementsByClassName('icon');
 
 button.addEventListener('click',() => {
 let city= input.value;
@@ -9,9 +10,8 @@ let city= input.value;
 $.getJSON(
     "http://api.openweathermap.org/data/2.5/forecast?q=Antwerpen&units=%20metric&appid=e9a2a366f60242dd2238a9c477ce36d3",(data) =>{
     let icon ="https://openweathermap.org/img/w/" + data.list[0].weather[0].icon +".png";
-    var temp = data.weather.temp;
 
-    $('.icon').attr('src',icon);
+    iconWeather[0].setAttribute('src',icon);
 });
 
 
