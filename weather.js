6e15bb48121a486ed5accbfb348a01b2015e8af2
iliastic <mesbahiilias1@gmail.com>
2019-07-04 15:49:38 +0200

    added temp and humidity

--- weather.js
+++ weather.js
@@ -2,22 +2,43 @@ const key='e9a2a366f60242dd2238a9c477ce36d3';
 let input=document.getElementById('search');
 let button=document.getElementById('search-btn');
 let iconWeather=document.getElementsByClassName('icon');
+let temper=document.getElementsByClassName('temp');
+let windD=document.getElementsByClassName('wind');
+let humid=document.getElementsByClassName('humidity');
 
 button.addEventListener('click',() => {
 let city= input.value;
-});
 
 $.getJSON(
-    "http://api.openweathermap.org/data/2.5/forecast?q=Antwerpen&units=%20metric&appid=e9a2a366f60242dd2238a9c477ce36d3",(data) =>{
+    `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=e9a2a366f60242dd2238a9c477ce36d3`,(data) =>{
+        
+        //icon
+        
+        for (let i= 0, d=0 ; i< iconWeather.length; i++, d+=8) {
+            var icon = data.list[d].weather[0].icon;  
+            iconWeather[i].setAttribute('src',"https://openweathermap.org/img/w/" + icon +".png");         
+            
+            //temperature
+            
+            temper[i].innerHTML= data.list[d].main.temp + "°C";
 
-    for (let i= 0, d=0 ; i< iconWeather.length; i++, d+=8) {
-        var icon = data.list[d].weather[0].icon;  
-        iconWeather[i].setAttribute('src',"https://openweathermap.org/img/w/" + icon +".png");         
-        console.log(icon);
-    } 
+            //humidity
 
+            humid[i].innerHTML=data.list[d].main.humidity + "% humidity";
+            console.log(data.list[d].main.humidity + "% humidity");
+
+             /* //wind
+
+             windD[i].innerHTML=data.list[0].wind;
+             console.log(data.list[0].wind)*/
+ 
+            
+        } 
+        
+        
+    });
 });
-
+    
 
 
 
